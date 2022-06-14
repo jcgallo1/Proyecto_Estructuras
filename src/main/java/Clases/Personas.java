59046c06a36043a76bf4fd2edd782dee4043a0b5
JuanDemera <jpdemera@espol.edu.ec>
2022-06-14 10:37:27 -0500

    completar foto

---
+++ src/main/java/Clases/Personas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author juan
+ */
+class Personas {
+    private String nombre;
+
+    public Personas(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
--- src/main/java/Clases/fotos.java
+++ src/main/java/Clases/fotos.java
@@ -5,10 +5,71 @@
  */
 package Clases;
 
+import TDAS.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author User
  */
 public class fotos {
     
+    private String descripcion;
+    private String lugar;
+    private Date fecha;
+    private ArrayList<Personas> persona;
+    private Album nombreAlbum;
+
+    public fotos(String descripcion, String lugar, 
+                Date fecha, 
+                ArrayList<Personas> persona, 
+                Album nombreAlbum) {
+        
+        this.descripcion = descripcion;
+        this.lugar = lugar;
+        this.fecha = fecha;
+        this.persona = persona;
+        this.nombreAlbum = nombreAlbum;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public ArrayList<Personas> getPersona() {
+        return persona;
+    }
+
+    public void setPersona(ArrayList<Personas> persona) {
+        this.persona = persona;
+    }
+
+    public Album getNombreAlbum() {
+        return nombreAlbum;
+    }
+
+    public void setNombreAlbum(Album nombreAlbum) {
+        this.nombreAlbum = nombreAlbum;
+    }
+     
 }
